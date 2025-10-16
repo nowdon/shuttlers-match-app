86315a72ab99d953f499e08c88b19584a229066a
YusukeImai <nowdon@gmail.com>
2025-10-16 16:05:43 +0900

    Fix save_match_state_full() to enable overwriting match_active value

--- utils/match_state.py
+++ utils/match_state.py
@@ -10,9 +10,9 @@ def load_match_state():
     with open(STATE_FILE, 'r') as f:
         return json.load(f)
 
-def save_match_state_full(matches, bench, match_count):
+def save_match_state_full(match_active, matches, bench, match_count):
     state = {
-        "match_active": True,
+        "match_active": match_active,
         "match_count": match_count,
         "matches": matches,
         "bench": bench,
